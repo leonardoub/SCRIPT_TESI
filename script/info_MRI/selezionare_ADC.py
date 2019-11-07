bb18c0de917a9d2f56751c2f10c204446a1ae92c
leonardoub <ubaldi.leonardo@gmail.com>
2019-11-07 18:02:08 +0100

    modifica script ADC

--- script/info_MRI/selezionare_ADC.py
+++ script/info_MRI/selezionare_ADC.py
@@ -8,7 +8,7 @@ This is a temporary script file.
 #voglio separare i volumi delle ADC dai volumi dei gradienti
 
 import pandas as pd
-import nibabel as nb
+import nibabel as nib
 import numpy as np
 import glob
 import os
@@ -23,31 +23,62 @@ path_img_ADC_list = [i for i in path_DWI_list if '_ADC' in i]
 
 path_img_list =[j.replace('_ADC','') for j in path_img_ADC_list]
 
-
-img_ADC = nb.load(A).get_fdata()
-shape_img_ADC = img_ADC.shape
-
-img = nb.load(B).get_fdata()
-shape_img = img.shape
-
-l = shape_img[-1]
-
-ADC_1 = img_ADC[:,:,:,l]
-shape_ADC_1 = ADC_1.shape
-
-T=np.transpose(ADC_1, [3,0,1,2])
-
-
-try:
+for path_img, path_img_ADC in zip(path_img_list, path_img_ADC_list):
     
-    ADC_2 = img_ADC[:,:,:,l+1]
-    shape_ADC_2 = ADC_2.shape
+    img = nib.load(path_img)
+    img_array = img.get_fdata()
+    shape_img = img.shape
     
-except IndexError:
-    pass
+    img_ADC = nib.load(path_img_ADC)
+    img_ADC_array = img_ADC.get_fdata()
+    shape_img_ADC = img_ADC.shape
+       
+    l = shape_img[-1]
 
+    ADC = img_ADC_array[:,:,:,l:]
 
-
+    T=np.transpose(ADC, [3,0,1,2]) 
+    
+    for i, single_ADC in enumerate(T):
+        
+        out_fname = path_img.replace('-a', f'ADC_{i+1}')
+        
+        single_ADC=single_ADC.astype('float32')
+        
+        nib.Nifti1Image(single_ADC, img_ADC.affine).to_filename(out_fname)
+  
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+#    
+#    
+#
+#    shape_ADC_1 = ADC_1.shape
+#    
+#    T=np.transpose(ADC_1, [3,0,1,2])
+#    
+#    
+#    try:
+#        
+#        ADC_2 = img_ADC[:,:,:,l+1]
+#        shape_ADC_2 = ADC_2.shape
+#        
+#    except IndexError:
+#        pass
+#    
+#    
+#
 
 
 
