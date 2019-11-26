507f0830a4e32094626ac85c0d42482800a9b1b2
leonardoub <ubaldi.leonardo@gmail.com>
2019-11-26 10:14:37 +0100

    add print

--- script/script_funzioni/estrattore_features_ADC.py
+++ script/script_funzioni/estrattore_features_ADC.py
@@ -23,7 +23,12 @@ def EstrattoreFeaturesADC(path_list, bin_Width):
         Output_path = list_path[i][2]
         Mask_path = list_path[i][1]
         Image_path = list_path[i][0]
-    
+        
+        print(Output_path)
+        print(Mask_path)
+        print(Image_path)
+        
+      
         Maskstk = sitk.ReadImage(Mask_path)
         Imagestk = sitk.ReadImage(Image_path)    
             
