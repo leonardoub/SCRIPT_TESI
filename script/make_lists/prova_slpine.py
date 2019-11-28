779700f80585cb32b55589cf3056b0dfa5eee4ad
leonardoub <ubaldi.leonardo@gmail.com>
2019-11-28 18:23:06 +0100

    add script

--- script/make_lists/prova_slpine.py
+++ script/make_lists/prova_slpine.py
@@ -9,9 +9,14 @@ Created on Wed Nov 27 19:58:43 2019
 import numpy as np
 import SimpleITK as sitk
 import argparse
+import matplotlib.pyplot as plt
+import scipy.stats 
 
 
-Mask_path='/home/leonardo/Scrivania/data_registered/data/Patient48/MASK_TOT.nii'
+stp=50
+
+
+Mask_path='/home/leonardo/Scrivania/data_registered/Patient56/MASK_TOT.nii'
 
 
 Maskstk = sitk.ReadImage(Mask_path)
@@ -21,9 +26,21 @@ num_slices = Masknp.shape[0]
 
 A = Masknp.sum(axis=(1,2)) #non c'è bisogno di un ciclo for sulle slices, lo fa da solo
 
+mask = [np.all(A[i-stp:i]==sorted(A[i-stp:i]) and A[i]!=0) for i in range(num_slices)]
+
+B = A[::-1]
+
+p = np.percentile(A, 87)
+
 #indices = np.where(A >= 450)
 #index = indices[0][0]
 #Masknp[:index,:,:] = 0
+#
+#plt.plot(A,'r+')
+#
 
 
 
+
+scipy.io()
+loadmat
